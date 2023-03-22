b76d617f3ba146284e532bc3c831b34b3720f310
Steven Hangger <LonghronShen@Hotmail.com>
2023-03-22 15:46:00 +0000

    chore: *
    *

--- download_models.sh
+++ download_models.sh
@@ -2,10 +2,4 @@
 
 set -x
 
-aria2c \
-    --https-proxy=$HTTPS_PROXY \
-    --http-proxy=$HTTP_PROXY \
-    --no-proxy=$NO_PROXY \
-    --input-file ./models/links.txt \
-    --dir ./models \
-    --continue
+aria2c --https-proxy=$HTTPS_PROXY --http-proxy=$HTTP_PROXY --no-proxy=$NO_PROXY --input-file ./models/links.txt --dir ./models --continue
